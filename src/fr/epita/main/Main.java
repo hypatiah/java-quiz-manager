3168f0a33c4d38798e77e3486342c123446f44d3
hypatiah <danielle.hassid@gmail.com>
2019-02-20 22:44:56 +0100

    add technical specification and user guide

--- src/fr/epita/main/Main.java
+++ src/fr/epita/main/Main.java
@@ -55,22 +55,6 @@ public class Main {
 		default:
 			System.out.println("Wrong input. Please enter either '1' for quiz manager and '2' for student");
 		}
-		
-		
-
-//		Doctor doctor = new Doctor("John Smith", 42);      // instantiation of doctor
-//		Patient patient = new Patient("Bobby Rickman", 28);  // instantiation of patient
-//		Nurse nurse = new Nurse("Marlene Simmons", 38);    // instantiation of nurse
-//		Appointment appointment = new Appointment(doctor, patient, new Date(), AppointmentType.OPERATION);
-//		// instantiation of appointment with doctor, patient, date and appointment type as parameters
-//		List<Nurse> nursesInvolved = new ArrayList<Nurse>();
-//		nursesInvolved.add(nurse); // put the nurse in a list
-//		
-//		appointment.setNursesInvolved(nursesInvolved); // add list of nurses to appointment
-//		
-//		for (Nurse nurseInvolved : appointment.getNursesInvolved()) { // for each nurse involved in the operation
-//			System.out.println("Nurse involved : " + nurseInvolved.getName());
-//		}
 	}
 	
 	/**
